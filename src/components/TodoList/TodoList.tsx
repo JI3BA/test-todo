5a75e1e48d6c6734d29b73f90222e9d6c62219ab
JI3BA <lyovkin_artyom@mail.ru>
2023-07-24 19:30:02 +0300

    added tagsContainer and worked with it

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,34 +1,47 @@
 import {Container, Grid} from "@mui/material";
-import {TodoTask} from "../TodoTask/TodoTask";
 import {useDispatch, useSelector} from "react-redux";
 import {removeTaskAC, TaskType} from "../../store/tasksReducer";
 import {AppRootState} from "../../store";
 import {AddItem} from "../AddItem/AddItem";
 import {Filter} from "../Filter/Filter";
+import {useEffect, useState, useMemo} from "react";
+import {TableTasks} from "../Table/TableTasks";
 
 
 export const TodoList = () => {
     const tasks = useSelector<AppRootState, TaskType[]>(state => state.tasks.tasks)
+    const [tagsContainer, setTagsContainer] = useState<string[]>([])
+    const [currentFilterTags, setCurrentFilterTags] = useState<string[]>([])
     const dispatch = useDispatch()
+
+    const filterNote = useMemo<TaskType[]>(() => {
+        if (currentFilterTags.length > 0) return tasks.filter((task) => currentFilterTags.some((tag) => task.tags.includes(tag)))
+        return tasks;
+    }, [tasks, currentFilterTags])
+
+    useEffect(() => {
+        const tagsFiltered: string[] = []
+        tasks.reduce((tags: string[], task) => [...tags, ...task.tags], []).map(tag => !tagsFiltered.includes(tag) ? tagsFiltered.push(tag) : null)
+
+        setTagsContainer(tagsFiltered);
+    }, [tasks])
 
     const removeTask = (taskId: number) => {
         dispatch(removeTaskAC(taskId))
     }
 
     return(
-            <Container fixed>
-                <Grid container sx={{ width: '370px', display: 'flex', justifyContent: 'center'}}>
+            <Container fixed sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Grid container sx={{ width: '450px', display: 'flex', justifyContent: 'center'}}>
                     <AddItem />
                 </Grid>
 
-                <Grid container sx={{ width: '345px', display: 'flex', justifyContent: 'center'}}>
-                    <Filter />
+                <Grid container sx={{ width: '450px', display: 'flex', justifyContent: 'center'}}>
+                    <Filter tags={tagsContainer} currentTags={currentFilterTags} setCurrentTags={setCurrentFilterTags} />
                 </Grid>
 
-                {tasks.length
-                    ? tasks.map(task => <TodoTask key={task.id} task={task} removeTask={removeTask}/>)
-                    : <p>Notes not found</p>
-                }
+                <TableTasks filterNote={filterNote} removeTask={removeTask}/>
+
             </Container>
     )
 }
