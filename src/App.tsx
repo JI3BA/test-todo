3d41b1f48bb0547004dd52108dd259d130e20e50
JI3BA <lyovkin_artyom@mail.ru>
2023-09-29 20:56:17 +0300

    AddList component was added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import {TodoList} from "./components/TodoList/TodoList";
 import {Provider} from "react-redux";
 import {store, persistor} from "./store";
 import { PersistGate } from 'redux-persist/integration/react'
+import { AddList } from "./components/AddList/AddList";
 
 
 function App() {
   return (
       <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-              <div className="App">
+              <div>
+                  <AddList />
                   <TodoList />
               </div>
           </PersistGate>
