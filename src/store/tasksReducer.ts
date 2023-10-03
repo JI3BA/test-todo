f0944de363555381f6a7f8fd842278319fcc478f
JI3BA <lyovkin_artyom@mail.ru>
2023-10-03 18:38:28 +0300

    taskReducer was created and tasks (defaultState) was added

--- src/store/tasksReducer.ts
+++ src/store/tasksReducer.ts
@@ -1,20 +1,6 @@
-export type TaskType = {
-    id: number,
-    task: string,
-    isDone: boolean,
-    tags: string[]
-}
-
-export type TasksState = {
-    tasks: TaskType[]
-}
-
-const defaultState = {
-    tasks:[
-
-    ]
-}
-
+import {ITasksState} from "../models/ITasks";
+import {v1} from "uuid";
+import {todolistId1, todolistId2} from "./todoListReducer";
 
 export type RemoveTaskActionType = ReturnType<typeof  removeTaskAC>
 export type AddTaskActionType = ReturnType<typeof addTaskAC>
@@ -23,27 +9,38 @@ export type changeTaskType = ReturnType<typeof changeTaskAC>
 
 type ActionsType = RemoveTaskActionType | AddTaskActionType | changeTaskType | changeTaskStatusType
 
-export const tasksReducer = (state: TasksState = defaultState, action: ActionsType): TasksState => {
+const tasks = {
+        [todolistId1]: [
+            {id: v1(), task: "HTML&CSS", isDone: true, tags: []},
+            {id: v1(), task: "JS #language", isDone: false, tags: ['language']}
+        ],
+        [todolistId2]: [
+            {id: v1(), task: "Milk and #bread", isDone: false, tags: ['bread']},
+            {id: v1(), task: "React Book", isDone: true, tags: []}
+        ]
+}
+
+export const tasksReducer = (state: ITasksState = tasks, action: ActionsType): ITasksState => {
     switch (action.type) {
         case 'REMOVE-TASK':
             return {
                 ...state,
-                tasks: state.tasks.filter(t => t.id !== action.taskId)
+                [action.todolistId]: state[action.todolistId].filter(t => t.id !== action.taskId)
             }
         case 'ADD-TASK':
             return {
                 ...state,
-                tasks: [{id: Date.now(), task: action.task, isDone:false, tags: [...action.tags]}, ...state.tasks]
+                [action.todolistId]: [{id: v1(), task: action.task, isDone: false, tags: [...action.tags]}, ...state[action.todolistId]]
             }
         case 'CHANGE-TASK-STATUS':
             return {
                 ...state,
-                tasks: state.tasks.map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : t)
+                [action.todolistId]: state[action.todolistId].map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : t)
             }
         case 'CHANGE-TASK':
             return {
                 ...state,
-                tasks: state.tasks.map(t => t.id === action.taskId ? {...t, task: action.task, tags: [...action.tags]} : t)
+                [action.todolistId]: state[action.todolistId].map(t => t.id === action.taskId ? {...t, task: action.task, tags: [...action.tags]} : t)
             }
         default:
             return state
@@ -51,17 +48,17 @@ export const tasksReducer = (state: TasksState = defaultState, action: ActionsTy
 }
 
 
-export const removeTaskAC = (taskId: number) => {
-    return { type: 'REMOVE-TASK', taskId} as const
+export const removeTaskAC = (todolistId: string, taskId: string) => {
+    return { type: 'REMOVE-TASK', todolistId, taskId} as const
 }
-export const addTaskAC = (task: string, tags: string[]) => {
-    return { type: 'ADD-TASK', task, tags} as const
-}
-
-export const changeTaskStatusAC = (taskId: number, isDone: boolean) => {
-    return { type: 'CHANGE-TASK-STATUS', taskId, isDone} as const
+export const addTaskAC = (todolistId: string, task: string, tags: string[]) => {
+    return { type: 'ADD-TASK',todolistId, task, tags} as const
 }
 
-export const changeTaskAC = (taskId: number, task: string, tags: string[]) => {
-    return { type: 'CHANGE-TASK', taskId, task, tags} as const
+export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => {
+    return { type: 'CHANGE-TASK-STATUS',todolistId, taskId, isDone} as const
+}
+
+export const changeTaskAC = (todolistId: string, taskId: string, task: string, tags: string[]) => {
+    return { type: 'CHANGE-TASK',todolistId, taskId, task, tags} as const
 }
