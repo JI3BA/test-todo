ed444631b7e5408e0b9c8e75d444cd9600364758
JI3BA <lyovkin_artyom@mail.ru>
2023-10-05 18:53:29 +0300

    To TableTasksType was added new param (todoListId) and it was transferred to TodoTask component

--- src/components/Table/TableTasks.tsx
+++ src/components/Table/TableTasks.tsx
@@ -8,16 +8,17 @@ import {
     TableRow
 } from "@mui/material";
 import {TodoTask} from "../TodoTask/TodoTask";
-import {TaskType} from "../../store/tasksReducer";
 import {ChangeEvent, FC, MouseEvent, useState} from "react";
 import {TablePaginationActions} from "./TablePaginationActions";
+import {ITasks} from "../../models/ITasks";
 
 type TableTasksType = {
-    filterNote: TaskType[],
-    removeTask: (taskId: number) => void
+    todoListsId: string,
+    filterNote: ITasks[],
+    removeTask: (todoListId: string, taskId: string) => void
 }
 
-export const TableTasks: FC<TableTasksType> = ({filterNote, removeTask}) => {
+export const TableTasks: FC<TableTasksType> = ({todoListsId, filterNote, removeTask}) => {
     const [page, setPage] = useState<number>(0);
     const [rowsPerPage, setRowsPerPage] = useState<number>(5);
 
@@ -43,7 +44,7 @@ export const TableTasks: FC<TableTasksType> = ({filterNote, removeTask}) => {
                         ? filterNote.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(task =>
                             <TableRow key={task.id} sx={{width: '100%'}}>
                                 <TableCell sx={{display: 'flex', padding: '0'}}>
-                                    <TodoTask task={task} removeTask={removeTask}/>
+                                    <TodoTask task={task} todoListsId={todoListsId} removeTask={removeTask}/>
                                 </TableCell>
                             </TableRow>
                         )
