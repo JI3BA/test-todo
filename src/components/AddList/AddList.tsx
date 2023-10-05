a56b9d1b3f650c37e194d6ca96524b290aed0012
JI3BA <lyovkin_artyom@mail.ru>
2023-10-05 18:44:46 +0300

    To AddList component was added functional for adding new List

--- src/components/AddList/AddList.tsx
+++ src/components/AddList/AddList.tsx
@@ -1,21 +1,52 @@
 import {IconButton, TextField} from "@mui/material";
 import {AddCircle} from "@mui/icons-material";
 import {AddItemForm} from "../Boxes/AddItemForm/AddItemForm";
+import {useDispatch} from "react-redux";
+import {addListAC} from "../../store/todoListReducer";
+import {KeyboardEvent, useState} from "react";
+import {v1} from "uuid";
+import {addListTaskAC} from "../../store/tasksReducer";
 
 
 export const AddList = () => {
+    const dispatch = useDispatch()
+    const [valueList , setValueList] = useState<string>('')
+    const [error, setError] = useState<string | null>(null)
+
+    const addList = () => {
+        const todoListId = v1()
+        if (valueList.trim() !== "") {
+            dispatch(addListAC(todoListId, valueList))
+            dispatch(addListTaskAC(todoListId))
+            setValueList('')
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addList()
+        }
+    }
+
     return(
         <AddItemForm>
             <TextField id="add-list"
                        label="ListName"
                        size='small'
+                       value={valueList}
+                       error={!!error}
+                       onChange={event => setValueList(event.target.value)}
+                       onKeyPress={onKeyPressHandler}
                        variant="outlined"
                        sx={{
                            width: '370px'
                        }}
             />
 
-            <IconButton color="primary" size='large'>
+            <IconButton color="primary" size='large' onClick={addList}>
                 <AddCircle fontSize="inherit" />
             </IconButton>
         </AddItemForm>
